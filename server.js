33a0d23793ebc35b6be2da51efe6c0083ed2798d
Mohammad Sadr <sadr@Mohammads-MacBook-Air.local>
2025-04-16 19:04:23 +0330

    Fix static files paths

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// سرو کردن فایل‌های استاتیک
+// سرو کردن فایل‌های استاتیک از پوشه‌های مختلف
 app.use(express.static(path.join(__dirname)));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // روت اصلی
 app.get('/', (req, res) => {
